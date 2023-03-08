b968e2cc94417eb9b60a28878a62101ff4a0654d
ghuba <nybachokua@gmail.com>
2023-03-08 16:48:39 +0200

    add colision detection

--- Models/Imp/Shape.cpp
+++ Models/Imp/Shape.cpp
@@ -6,21 +6,21 @@
 void Shape::move_coordinates(Direction direction) {
     switch (direction) {
         case Direction::Down:
-            this->y++;
+            this->_point.y++;
             break;
         case Direction::Left:
-            this->x--;
+            this->_point.x--;
             break;
         case Direction::Right:
-            this->x++;
+            this->_point.x++;
             break;
         default:
-            break;
+            static_assert("Impossible direction to movement");
     }
 }
 
 bool Shape::check_space(Matrix& matrix) {
-    for(Point p: this->placement)
+    for(Point p: this->_placement)
         if(matrix.get_point(p.x, p.y))
             return false;
 
@@ -31,12 +31,57 @@ Matrix Shape::add_to_matrix(Matrix& matrix) {
     if(!check_space(matrix))
         throw std::exception{};
 
-    for(auto point : this->placement)
+    for(auto point : this->_placement)
         matrix.set_point(point.x, point.y);
 
     return matrix;
 }
 
-void Shape::move(Direction direction, Matrix &matrix) {
+bool Shape::check_collision(Direction direction, Matrix &matrix) {
 
+    int xKoef {0}, yKoef {0};
+
+    switch (direction) {
+        case Direction::Down:
+            yKoef=1;
+            break;
+        case Direction::Left:
+            xKoef = -2;
+            break;
+        case Direction::Right:
+            xKoef = 1;
+            break;
+        default:
+            static_assert("Impossible direction to movement");
+    }
+
+    for(Point& point: _placement){
+        int x = this->_point.x+point.x+xKoef;
+        int y = this->_point.y+point.y+yKoef;
+        if (matrix.get_point(x, y))
+            return true;
+    }
+
+    return false;
 }
+
+bool Shape::move(Matrix &matrix) {
+    if(check_collision(Direction::Down,matrix))
+        return false;
+
+    move_coordinates(Direction::Down);
+
+    return true;
+}
+
+bool Shape::move_horizontal(Matrix &matrix, bool isRight) {
+    Direction direction = isRight ? Direction::Right : Direction::Left;
+    if(check_collision(direction,matrix))
+        return false;
+
+    move_coordinates(direction);
+
+    return true;
+}
+
+
--- Models/Shape.h
+++ Models/Shape.h
@@ -16,27 +16,59 @@ enum class Direction : char {
 };
 
 enum class Rotation{
-    Vertical,
-    Horizontal
+    HorizontalRight,
+    HorizontalLeft,
+    VerticalUp,
+    VerticalDown
+};
+
+enum class ShapeType{
+    Cube,
+    Long,
+    Triangle,
+    Harpoon
 };
 
 struct Point{
     int x, y;
+    Point(int _x, int _y) : x {_x}, y {_y} { };
     Point() : x {0}, y {0} { }
 };
 
 class Shape {
 private:
     Point _point;
-    Rotation rotation;
-    std::vector<Point> placement;
+    Rotation _rotation;
+    std::vector<Point> _placement;
 
     bool check_space(Matrix& matrix);
+    bool check_collision(Direction direction, Matrix& matrix);
     void move_coordinates(Direction direction);
     Matrix add_to_matrix(Matrix& matrix);
 public:
-    Shape() : _point(), rotation { Rotation::Horizontal} { };
-    void move(Direction direction, Matrix& matrix);
+    explicit Shape(ShapeType type) : _point(), _rotation {Rotation::HorizontalRight} {
+        switch (type) {
+
+            case ShapeType::Cube:
+                _placement = { Point{ 0, 0}, Point{ 0, 1}, Point{ 1, 0}, Point{ 1, 1}};
+                break;
+            case ShapeType::Long:
+                _placement = { Point { 0, 0 }, Point { 1, 0 }, Point { 2, 0 }, Point { 4, 0 }};
+                break;
+            case ShapeType::Triangle:
+                throw std::exception();
+                break;
+            case ShapeType::Harpoon:
+                throw std::exception();
+                break;
+            default:
+                _placement = { Point{ 0, 0}, Point{ 0, 1}, Point{ 1, 0}, Point{ 1, 1}};
+                break;
+        }
+    };
+
+    bool move(Matrix& matrix);
+    bool move_horizontal(Matrix& matrix, bool isRight = true)
 };
 
 #endif //SRC_SHAPE_H
